f17e2a9dc7f996cc7cf83ddba16e4b8ca5210495
Stephan Schöpe <socialcrap+github@posteo.de>
2021-10-17 18:46:53 +0200

    production entrypoint corrected

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -4,7 +4,7 @@ npm install
 if [ $NODE_ENV == "production" ]
 then
     # https://github.com/nodejs/docker-node/blob/main/docs/BestPractices.md#cmd
-    node app.js 
+    node index.js 
 else
     npm run serve
 fi
