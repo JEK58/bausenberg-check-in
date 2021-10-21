6c0b86fd062df96c3986a6875214f3a9e03ac494
Stephan Schöpe <socialcrap+github@posteo.de>
2021-10-21 18:50:25 +0200

    changed from npm to yarn

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-npm install
+yarn install
 
 if [ $NODE_ENV == "production" ]
 then
     # https://github.com/nodejs/docker-node/blob/main/docs/BestPractices.md#cmd
     node index.js 
 else
-    npm run serve
+    yarn serve
 fi
