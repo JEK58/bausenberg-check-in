f747681337b320afee09af56b0c0a39e66e38e3b
Stephan Schöpe <socialcrap+github@posteo.de>
2021-12-29 13:40:54 +0100

    pr comments implemented

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const http = require("http");
 const server = http.createServer(app);
 
 // Middleware
-app.use(express.json({ limit: "5mb" }));
+app.use(express.json());
 app.use(cors());
 
 // Rate Limit
