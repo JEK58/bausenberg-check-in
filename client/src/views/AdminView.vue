bf1b7540f815742bc35b6ed15578999fdb2c8a40
Stephan Schöpe <socialcrap+github@posteo.de>
2022-01-03 21:27:17 -0600

    date-fns-tz added

--- client/src/views/AdminView.vue
+++ client/src/views/AdminView.vue
@@ -36,93 +36,94 @@
           </li>
         </ul>
       </nav>
+      <article>
+        <h6>
+          Gesamt: <strong>{{ dataToShow?.length }}</strong>
+        </h6>
 
-      <h6>
-        Gesamt: <strong>{{ dataToShow?.length }}</strong>
-      </h6>
-
-      <ul>
-        <li>
-          Landewiese: <strong>{{ statistics?.regularLanding ?? 0 }}</strong>
-        </li>
-        <li>
-          Notlandewiese:
-          <strong>{{ statistics?.alternateLanding ?? 0 }}</strong>
-        </li>
-        <li>
-          Streckenflug:<strong> {{ statistics?.xcLanding ?? 0 }}</strong>
-        </li>
-        <li>
-          Nicht gestartet:
-          <strong>{{ statistics?.didNotStart ?? 0 }}</strong>
-        </li>
-        <li>
-          Landung nicht gemeldet:
-          <strong>{{ statistics?.notReported ?? 0 }}</strong>
-        </li>
-      </ul>
-
+        <ul>
+          <li>
+            Landewiese: <strong>{{ statistics?.regularLanding ?? 0 }}</strong>
+          </li>
+          <li>
+            Notlandewiese:
+            <strong>{{ statistics?.alternateLanding ?? 0 }}</strong>
+          </li>
+          <li>
+            Streckenflug:<strong> {{ statistics?.xcLanding ?? 0 }}</strong>
+          </li>
+          <li>
+            Nicht gestartet:
+            <strong>{{ statistics?.didNotStart ?? 0 }}</strong>
+          </li>
+          <li>
+            Landung nicht gemeldet:
+            <strong>{{ statistics?.notReported ?? 0 }}</strong>
+          </li>
+        </ul>
+      </article>
       <h4>Liste</h4>
-
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Verein</th>
-            <th>Landung</th>
-            <th>Datum</th>
-            <th>
-              <a href="#" @click="fetchDB"
-                ><svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-arrow-repeat"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"
-                  />
-                  <path
-                    fill-rule="evenodd"
-                    d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"
-                  />
-                </svg>
-              </a>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="checkIn in dataToShow" :key="checkIn._id">
-            <td>{{ checkIn.name }}</td>
-            <td>{{ checkIn.club }}</td>
-            <td>{{ checkIn.landing }}</td>
-            <!-- TODO: Ignore timezone… -->
-            <td>{{ formatDate(checkIn.checkInDate) }}</td>
-            <td>
-              <a href="#" class="warning" @click="onDeleteEntry(checkIn)">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="14"
-                  height="14"
-                  fill="currentColor"
-                  class="bi bi-trash"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"
-                  />
-                  <path
-                    fill-rule="evenodd"
-                    d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
-                  />
-                </svg>
-              </a>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <article>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Verein</th>
+              <th>Landung</th>
+              <th>Datum</th>
+              <th>
+                <a href="#" @click="fetchDB"
+                  ><svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-arrow-repeat"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"
+                    />
+                    <path
+                      fill-rule="evenodd"
+                      d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"
+                    />
+                  </svg>
+                </a>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="checkIn in dataToShow" :key="checkIn._id">
+              <td>{{ checkIn.name }}</td>
+              <td>{{ checkIn.club }}</td>
+              <td>{{ checkIn.landing }}</td>
+              <!-- TODO: Ignore timezone… -->
+              <td>{{ formatDate(checkIn.checkInDate) }}</td>
+              <td>
+                <a href="#" class="warning" @click="onDeleteEntry(checkIn)">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="14"
+                    height="14"
+                    fill="currentColor"
+                    class="bi bi-trash"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"
+                    />
+                    <path
+                      fill-rule="evenodd"
+                      d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+                    />
+                  </svg>
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </article>
     </div>
     <div v-else></div>
     <!-- Login -->
@@ -194,10 +195,10 @@
 
 <script setup>
 import API from "@/services/API";
-import { format } from "date-fns";
 import { ref, computed } from "vue";
 import Swal from "sweetalert2";
 import "sweetalert2/dist/sweetalert2.css";
+import { formatInTimeZone, format } from "date-fns-tz";
 
 const dbData = ref(null);
 const loggedIn = ref(false);
@@ -300,7 +301,11 @@ const deleteEntry = async () => {
 // Format date
 const formatDate = (timestamp) => {
   if (!timestamp) return "-";
-  return format(new Date(timestamp), "dd.MM.yy - HH:mm");
+  return formatInTimeZone(
+    new Date(timestamp),
+    "Europe/Berlin",
+    "dd.MM.yy - HH:mm"
+  );
 };
 
 /*
