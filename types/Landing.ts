e101529bd8d554d2918a8ca0b7e2e27c50ac14d6
Stephan Schöpe <socialcrap+github@posteo.de>
2023-03-03 22:25:31 +0100

    deprecated type removed

--- types/Landing.ts
+++
@@ -1,7 +0,0 @@
-type Landing = "REGULAR" | "ALT" | "XC" | "DNF";
-// type Landing =
-//   | "Landewiese"
-//   | "Notlandewiese"
-//   | "Streckenflug"
-//   | "Doch nicht gestartet";
-export default Landing;
