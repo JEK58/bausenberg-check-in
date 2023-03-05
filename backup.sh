db7910c36c7eef64e64372b5c603e2544700bd07
Stephan Schöpe <socialcrap+github@posteo.de>
2023-03-05 21:30:00 +0100

    backup script refined

--- backup.sh
+++ backup.sh
@@ -1,19 +1,62 @@
 #!/bin/bash
-set -e #Exit if any command failed with != 0
 
-docker exec -i bausenberg-check-in-db /bin/bash -c "PGPASSWORD=$POSTGRES_PW \
-         pg_dump --username $POSTGRES_USER $POSTGRES_DB -f pg_dump.sql"
+# Define the container and database names
+CONTAINER_NAME=
+DATABASE_NAME=
 
-echo "Dump OK"
+# Define DB credentials
+PG_USER=
+PG_PASSWORD=
 
-mkdir -p /home/rml/pg-backups
-cd /home/rml/pg-backups
+# Define the SFTP server details
+SFTP_USER=
+SFTP_HOST=
+SFTP_PORT=
+SFTP_REMOTE_DIR=
+SFTP_PASSWORD=
 
-docker cp bausenberg-check-in-db:/pg_dump.sql .
-echo  "Copy from docker OK"
+# Get the current date and time
+DATE=$(date +%Y-%m-%d-%H-%M-%S)
 
-filename=backup_$(date +"%Y-%m-%d").zip
-zip -r $filename pg_dump.sql
-rm -rf pg_dump.sql
-sshpass -p '$PASSWORD' sftp $USER@$HOST:/ <<<$'put '$filename
-echo  "All done"
+# Create a directory for the backups
+mkdir -p backups
+
+# Create the backup file name
+BACKUP_FILE="backups/${DATABASE_NAME}-${DATE}.sql"
+
+# Use `docker exec` to run the `pg_dump` command in the container and write the output to a file
+docker exec -t ${CONTAINER_NAME} pg_dump -U ${PG_USER} -w ${DATABASE_NAME} > ${BACKUP_FILE}
+
+# Check the exit status of the pg_dump command
+if [ "$?" -eq 0 ]; then
+  echo "Backup successful: ${BACKUP_FILE}"
+else
+  echo "Backup failed"
+  exit 1
+fi
+
+# Compress the backup file
+echo "Compressing backup file..."
+gzip ${BACKUP_FILE}
+BACKUP_FILE="${BACKUP_FILE}.gz"
+
+# Upload the backup file to the remote SFTP server
+# Needs sshpass to be installed... 
+# sudo apt-get install sshpass
+
+echo "Uploading backup file to SFTP server..."
+sshpass -p ${SFTP_PASSWORD} sftp -P ${SFTP_PORT} ${SFTP_USER}@${SFTP_HOST}:${SFTP_REMOTE_DIR} <<< "put ${BACKUP_FILE}"
+
+# Check the exit status of the SFTP command
+if [ "$?" -eq 0 ]; then
+  echo "Upload successful"
+else
+  echo "Upload failed"
+  exit 1
+fi
+
+# Cleanup the local backup file
+echo "Cleaning up local backup file..."
+rm ${BACKUP_FILE}
+
+echo "Backup and upload complete"
