bad57c33dee4bf2bfdd3806159862a4cab1e40ca
Stephan Schöpe <socialcrap+github@posteo.de>
2023-03-05 18:54:46 +0100

    backup script modified for postgres

--- backup.sh
+++ backup.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 set -e #Exit if any command failed with != 0
 
-docker exec -i bausenberg-check-in-db /usr/bin/mongodump --username $DB_USER --password $DB_PW \
-	 --authenticationDatabase admin --db $DB --out /dump
+docker exec -i bausenberg-check-in-db /bin/bash -c "PGPASSWORD=$POSTGRES_PW \
+         pg_dump --username $POSTGRES_USER $POSTGRES_DB -f pg_dump.sql"
+
 echo "Dump OK"
 
-mkdir -p /home/rml/mongo-backups
-cd /home/rml/mongo-backups
+mkdir -p /home/rml/pg-backups
+cd /home/rml/pg-backups
 
-docker cp bausenberg-check-in-db:/dump .
+docker cp bausenberg-check-in-db:/pg_dump.sql .
 echo  "Copy from docker OK"
 
 filename=backup_$(date +"%Y-%m-%d").zip
-zip -r $filename dump
-rm -rf dump
+zip -r $filename pg_dump.sql
+rm -rf pg_dump.sql
 sshpass -p '$PASSWORD' sftp $USER@$HOST:/ <<<$'put '$filename
 echo  "All done"
