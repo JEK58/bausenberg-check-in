08c54f7eb79fb6a798dd2719bd15ac48999fffd5
Stephan Schöpe <socialcrap+github@posteo.de>
2023-03-06 22:02:27 +0100

    imports removed

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,10 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Home from "@/pages/index";
-import {
-  localStorageHasDataFromToday,
-  saveIdToLocalStorage,
-} from "@/util/local-storage";
+import { saveIdToLocalStorage } from "@/util/local-storage";
 
 jest.mock("next/router", () => ({
   useRouter: jest.fn(),
