97219b35da9223ac837ba9657800a727d8ef79ac
Stephan Schöpe <socialcrap+github@posteo.de>
2023-03-06 22:01:47 +0100

    tests expanded

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,10 +1,21 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Home from "@/pages/index";
+import {
+  localStorageHasDataFromToday,
+  saveIdToLocalStorage,
+} from "@/util/local-storage";
 
 jest.mock("next/router", () => ({
   useRouter: jest.fn(),
 }));
+
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
+const useRouter = jest.spyOn(require("next/router"), "useRouter");
+const router = { push: jest.fn() };
 
 describe("Home", () => {
   it("lets user check in if minimum details are given", async () => {
@@ -22,5 +33,22 @@ describe("Home", () => {
 
     await user.click(screen.getByLabelText("RML"));
     expect(screen.getByRole("button")).toBeEnabled();
+  });
+
+  it("redirects user to the check-out page if an unfinished check-in from today is present in local storage", () => {
+    saveIdToLocalStorage("some-id", new Date().toDateString());
+    useRouter.mockReturnValue(router);
+
+    render(<Home />);
+    expect(router.push).toHaveBeenCalledWith("/check-out");
+  });
+
+  it("lets the user check-in if there is an unfinished check-in is present in local storage that is NOT from today", () => {
+    const yesterday = new Date(Date.now() - 86400000).toDateString();
+    saveIdToLocalStorage("some-id", yesterday);
+    useRouter.mockReturnValue(router);
+
+    render(<Home />);
+    expect(router.push).not.toHaveBeenCalledWith("/check-out");
   });
 });
--- jest.setup.js
+++ jest.setup.js
@@ -4,3 +4,32 @@
 // Used for __tests__/testing-library.js
 // Learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
+
+const localStorageMock = (function () {
+  let store = {};
+
+  return {
+    getItem(key) {
+      return store[key];
+    },
+
+    setItem(key, value) {
+      store[key] = value;
+    },
+
+    clear() {
+      store = {};
+    },
+
+    removeItem(key) {
+      delete store[key];
+    },
+
+    getAll() {
+      return store;
+    },
+  };
+})();
+
+Object.defineProperty(window, "localStorage", { value: localStorageMock });
+global.localStorage;
